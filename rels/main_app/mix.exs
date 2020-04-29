2a4b491b1a3d6db4c5c415449d01400876b8602b
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2020-04-29 23:49:08 +0200

    Import config of Phoenix project.

--- rels/main_app/mix.exs
+++ rels/main_app/mix.exs
@@ -23,7 +23,7 @@ defmodule MainApp.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-	{:phoenix_app, path: "../../apps/phoenix_app"},
+	{:phoenix_app, path: "../../apps/phoenix_app", env: Mix.env()},
     ]
   end
 end
